a9ed7bb39b6277bc046a6c0bbdb724e83b13745e
ShingaiJP <spedzisayi@retrorabbit.co.za>
2018-01-08 07:42:15 +0200

    Resolved error with regards to Login logo location

--- v-tech-app/src/containers/login/index.js
+++ v-tech-app/src/containers/login/index.js
@@ -6,11 +6,13 @@ import { Route, Link } from 'react-router-dom'
 //import { Button } from 'reactstrap';
 import './login.css'
 
+
 const App = () => (
+
 
   <div class="form_area">
     <p>Welcome to the</p>
-      <img src={require("../../images/Full_Logo.png")} alt={"ChatterBox logo"} />
+      <img src={require("../login/Full_Logo.png")} alt={"ChatterBox logo"} />
         <form>
           <input type="email" class="form-control" id="email_add" placeholder="Email"></input>
           <input type="password" class="form-control" id="pswd" placeholder="Password"></input>
@@ -19,5 +21,6 @@ const App = () => (
     <Link to="/"><div class="register">No account yet? Get setup now</div></Link>
   </div>
 )
+
 
 export default App
