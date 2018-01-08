770f748712edd220d15209d42b827986119b2ae3
ShingaiJP <spedzisayi@retrorabbit.co.za>
2018-01-08 22:20:49 +0200

    Uniquely applied page designs and set up navigation between 'Login' and registration pages.

--- v-tech-app/src/App.js
+++ v-tech-app/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from 'react-router-dom'
+import ReactDOM from 'react-dom'
+import {
+  NavLink, 
+  HashRouter
+} from "react-router-dom";
+
+import Home from './containers/login'
+import Lgn_one from './containers/lgn_one'
+import Lgn_two from './containers/lgn_two'
+import Lgn_three from './containers/lgn_three'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+      
+    </div>
     );
   }
 }
