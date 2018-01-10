4355c3444ca64689382330ca5d0adb9d69e6468e
ReaMohulatsi <rmohulatsi@retrorabbit.co.za>
2018-01-10 16:19:01 +0200

    Updated the login container component to include login state and actions

--- v-tech-app/src/containers/login/index.js
+++ v-tech-app/src/containers/login/index.js
@@ -1,53 +1,78 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import './css_lgn/login.css'
+import {login_successful, login_unsuccessful} from '../../modules/loginreducer'
 
-const Home = props => (
-  <div class="contain">
-  <div class="lgn_form_area">
-    <h3 class="lgn_welcome">Welcome to the</h3>
-    <img src={require("../login/Full_Logo.png")} alt={"ChatterBox logo"} />
-    <form>
-      <input type="email" class="form-control lgn_field" id="email_add" placeholder="Email"></input>
-      <input type="password" class="form-control lgn_field" id="pswd" placeholder="Password"></input>
-      <button type="submit" id="login" class="btn btn-primary lgn_login">LOGIN</button>
-    </form>
-    <Link to="/register"><p class="lgn_register">No account yet? Get setup now</p></Link>
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {email:"",password:""};
+    this.checkDetails = this.checkDetails.bind(this);
+    this.updateInputEmail = this.updateInputEmail.bind(this);
+    this.updateInputPassword = this.updateInputPassword.bind(this);
+  }
 
-  </div>
-  </div>
-)
+  render(){
+    return (
+    <div class="contain">
+    <div class="lgn_form_area">
+      <h3 class="lgn_welcome">Welcome to the</h3>
+      <img src={require("../login/Full_Logo.png")} alt={"ChatterBox logo"} />
+      <form>
+        <input type="email" class="form-control lgn_field" name="aa" id="email_add" placeholder="Email" value={this.state.email} onChange={this.updateInputEmail}></input>
+        <input type="password" class="form-control lgn_field" id="pswd" placeholder="Password" value={this.state.password} onChange={this.updateInputPassword}></input>
+        <button type="submit" id="login" class="btn btn-primary lgn_login" disabled={this.props.isLogInPending} onClick={this.checkDetails}>LOGIN</button>
+      </form>
+      <Link to="/register"><p class="lgn_register">No account yet? Get setup now</p></Link>
+  
+    </div>
+    </div>)
+  }
+
+  checkDetails() {
+    if (this.state.email === "me@admin" && this.state.password === "admin") {
+      this.props.login_successful();
+      this.props.changePage();
+    } else {
+      this.props.login_unsuccessful();
+    }
+    this.setState({
+      email: '',
+      password: ''
+    });
+  }
+
+  updateInputEmail(event) {
+    this.setState({
+      email: event.target.value
+    });
+  }
+
+  updateInputPassword(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+}
+
+const mapStateToProps = state => ({
+  isLogInPending: state.loginreducer.isLogInPending,
+  isLogInSucess: state.loginreducer.isLogInSucess,
+  isLogInFail: state.loginreducer.isLogInFail
+})
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  changePage: () => push('/register')
+  login_successful,
+  login_unsuccessful,
+  changePage: () => push('/chats')
 }, dispatch)
 
 export default connect(
-  null, 
+  mapStateToProps, 
   mapDispatchToProps
 )(Home)
-
-/*
-const App = () => (
-
-
-
-  <div class="form_area">
-    <p>Welcome to the</p>
-      <img src={require("../login/Full_Logo.png")} alt={"ChatterBox logo"} />
-        <form>
-          <input type="email" class="form-control" id="email_add" placeholder="Email"></input>
-          <input type="password" class="form-control" id="pswd" placeholder="Password"></input>
-          <button type="submit" id="login" class="btn btn-primary">LOGIN</button>
-        </form>
-    <Link to="/register"><div class="register">No account yet? Get setup now</div></Link>
-  
-    <Route exact path="/register" component={Lgn_one} />
-
-  </div>
-)
-
-export default App*/
